e22a2adedaf41e6312952ffdd5529a41ccbec849
Lain <hhichri60@gmail.com>
2025-03-10 23:18:07 +0100

    simple class

--- src/dl3/text_generation/gpt2.py
+++ src/dl3/text_generation/gpt2.py
@@ -1,6 +1,19 @@
 import torch.nn as nn
+from huggingface_hub import PyTorchModelHubMixin
 
 
-class GPT2(nn.Module):
-    def __init__(self):
+class GPT2(
+    nn.Module,
+    PyTorchModelHubMixin,
+    library_name="dl3",
+    repo_url="https://github.com/not-lain/dl3",
+    tags=["gpt2", "text-generation"],
+):
+    def __init__(self, a, b):
         super().__init__()
+        self.l = nn.Linear(a, b)
+
+
+if __name__ == "__main__":
+    model = GPT2(10, 10)
+    print(model)
